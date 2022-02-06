7ef141b861aa380c80b8631969587720f9254348
Jack Gentile <jack.t.gentile@gmail.com>
2022-02-06 11:06:38 -0600

    make grid from keyword

--- vigenere.py
+++ vigenere.py
@@ -4,9 +4,26 @@ class Vigenere:
         self.make_grid()
 
     def make_grid(self):
-        pass
+        alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+        my_alphabet = ''
+        for c in self.key:
+            if c not in my_alphabet:
+                my_alphabet += c
+        for c in alphabet:
+            if c not in my_alphabet:
+                my_alphabet += c
+        self.grid = []
+        for i in range(0,25,5):
+            self.grid.append(my_alphabet[i:i+5])
+        self.coords = {}
+        for row in range(5):
+            for col in range(5):
+                self.coords[self.grid[row][col]] = (row, col)
 
     def swap(self, a, b):
+        return ''
+
+    def unswap(self, a, b):
         return ''
 
     def encrypt(self, plaintext):
@@ -19,9 +36,9 @@ class Vigenere:
 
 
 def main():
-    keyword = 'violet'
+    keyword = 'VIOLET'
     vig = Vigenere(keyword)
-    ciphertext = vig.encrypt('maverick')
+    ciphertext = vig.encrypt('MAVERICK')
     print(ciphertext)
 
 
